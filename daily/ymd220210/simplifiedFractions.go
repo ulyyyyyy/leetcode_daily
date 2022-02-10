53648f34cd157b93fd80c74b724a45ce8a727ba1
ulyyyyyy <gzw736118298@qq.com>
2022-02-10 21:16:45 +0800

    day20 - 22/02/10 暴力解，重点在于找到最大公约数

---
+++ daily/ymd220210/simplifiedFractions.go
@@ -0,0 +1,31 @@
+package ymd220210
+
+import (
+	"fmt"
+)
+
+// 给你一个整数 n ，请你返回所有 0 到 1 之间（不包括 0 和 1）满足分母小于等于  n 的 最简 分数 。分数可以以 任意 顺序返回。
+func simplifiedFractions(n int) (rlts []string) {
+	for i := 2; i <= n; i++ {
+		for j := 1; j < i; j++ {
+			if getMax(i, j) {
+				continue
+			}
+			rlts = append(rlts, fmt.Sprintf("%d/%d", j, i))
+
+		}
+	}
+	return
+}
+
+// 判断是否有最大公倍数
+func getMax(m, n int) bool {
+	for {
+		r := m % n
+		if r == 0 {
+			break
+		}
+		m, n = n, m%n
+	}
+	return n != 1
+}
